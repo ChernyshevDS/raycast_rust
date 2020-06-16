3835f893c6f47bca0e841dbcd95ec3c7751b0b8b
ChernyshevDS92 <j.phyzic@gmail.com>
2020-06-16 22:05:20 +0300

    added debug configuration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
+//https://habr.com/ru/post/436790/
+
 fn main() {
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input){
+        Ok(n) => {
+            println!("{} bytes read", n);
+        },
+        Err(e) => {
+            println!("error: {}", e);
+        }
+    }
     println!("Hello, world!");
 }
