2930454aa27a78f3dcb605db521f27f8e4354d50
ChernyshevDS92 <j.phyzic@gmail.com>
2020-06-18 10:24:31 +0300

    added first sphere-ray intersection

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,53 @@
 use std::fs::File;
 use std::io::prelude::*;
 use cgmath::prelude::*;
+use cgmath::Vector3;
+
+type Vec3f = Vector3<f32>;
 
 mod frame;
 
-fn main() {
-    let mut framebuffer = frame::Frame::new(640, 480);
+const FOV: f32 = std::f32::consts::FRAC_PI_2;
 
-    render(&mut framebuffer);
+fn main() {
+    let mut framebuffer = frame::Frame::new(1024, 768);
+    let sphere = Sphere { center: Vector3::new(-3.0, 0.0, -16.0), radius: 2.0 };
+
+    render(&mut framebuffer, &sphere);
 
     save_as_ppm(&framebuffer).unwrap();
 }
 
-fn render(framebuffer: &mut frame::Frame){
+fn render_gradient(framebuffer: &mut frame::Frame){
     for y in 0..framebuffer.height() {
         for x in 0..framebuffer.width() {
             let color = frame::ColorF::rgb(y as f32 / framebuffer.height() as f32,
                                            x as f32 / framebuffer.width() as f32, 
                                            0.0);
             framebuffer.set_pixel(x, y, &color);
+        }
+    }
+}
+
+fn cast_ray(orig: &Vec3f, dir: &Vec3f, sphere: &Sphere) -> frame::ColorF {
+    let mut sphere_dist = std::f32::MAX;
+    if !sphere.ray_intersect(orig, dir, &mut sphere_dist) {
+        return frame::ColorF::rgb(0.2, 0.7, 0.8);
+    }
+    return frame::ColorF::rgb(0.4, 0.4, 0.3);
+}
+
+fn render(framebuffer: &mut frame::Frame, sphere: &Sphere) {
+    let height = framebuffer.height(); 
+    let width = framebuffer.width();
+    let fovtan = (FOV / 2.0).tan();
+    for j in 0..height {
+        for i in 0..width {
+            let x =  (2.0 * (i as f32 + 0.5) / (width as f32)  - 1.0) * fovtan * (width as f32) / (height as f32);
+            let y = -(2.0 * (j as f32 + 0.5) / (height as f32) - 1.0) * fovtan;
+            let dir: Vec3f = Vector3::new(x, y, -1.0).normalize();
+            let color = cast_ray(&Vector3::zero(), &dir, sphere);
+            framebuffer.set_pixel(i, j, &color);
         }
     }
 }
@@ -65,4 +94,31 @@ fn save_as_ppm(frame: &frame::Frame) -> std::io::Result<()>{
     f.sync_all()?;
 
     Ok(())
+}
+
+pub struct Sphere {
+    pub center: Vec3f,
+    pub radius: f32
+}
+
+impl Sphere {
+    pub fn ray_intersect(&self, origin: &Vec3f, dir: &Vec3f, step: &mut f32) -> bool {
+        let L: Vec3f = self.center - origin;
+        let tca: f32 = L.dot(*dir);
+        let d2: f32 = L.dot(L) - tca*tca;
+        if d2 > self.radius * self.radius 
+        {
+            return false;
+        }
+        let thc: f32 = (self.radius*self.radius - d2).sqrt();
+        *step = tca - thc;
+        let t1 = tca + thc;
+        if *step < 0.0 {
+            *step = t1;
+        } 
+        if *step < 0.0 {
+            return false;
+        }
+        return true;
+    }
 }
